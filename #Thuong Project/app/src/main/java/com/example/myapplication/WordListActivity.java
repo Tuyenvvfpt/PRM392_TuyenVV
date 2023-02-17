1f619de3e075523a18943a4dc8723965dd7f5abe
Vu Van Tuyen <81419313+Tuyenvvfpt@users.noreply.github.com>
2023-02-17 12:15:22 +0700

    17022023
    end.

--- #Thuong Project/app/src/main/java/com/example/myapplication/WordListActivity.java
+++ #Thuong Project/app/src/main/java/com/example/myapplication/WordListActivity.java
@@ -10,6 +10,7 @@ import com.example.myapplication.adapter.WordListAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class WordListActivity extends AppCompatActivity {
     private List<String> wordList = new ArrayList<>();
@@ -25,10 +26,22 @@ public class WordListActivity extends AppCompatActivity {
         //wordListAdapter.notifyDataSetChanged(); => method used to update RecycleView
     }
 
+//    private List getWordList() {
+//        for (int i = 0; i <= 50; i++) {
+//            wordList.add("Word " + i);
+//        }
+//        return wordList;
+//    }
+
+
+    //17022023 do data storage
     private List getWordList() {
-        for (int i = 0; i <= 50; i++) {
-            wordList.add("Word " + i);
+        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.words));
+        while (scanner.hasNext()) {
+            String word = scanner.nextLine();
+            wordList.add(word);
         }
+        scanner.close();
         return wordList;
     }
 }
