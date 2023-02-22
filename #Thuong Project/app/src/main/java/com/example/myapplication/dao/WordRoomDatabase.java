865f62d4a8a8cac241db5f60d739b987781c3ff1
Vu Van Tuyen <81419313+Tuyenvvfpt@users.noreply.github.com>
2023-02-22 16:04:49 +0700

    22022023
    update WordRoomDatabase.java

--- #Thuong Project/app/src/main/java/com/example/myapplication/dao/WordRoomDatabase.java
+++ #Thuong Project/app/src/main/java/com/example/myapplication/dao/WordRoomDatabase.java
@@ -8,13 +8,15 @@ import androidx.room.RoomDatabase;
 
 import com.example.myapplication.entity.Word;
 
-@Database(entities = {Word.class}, version = 1 )
+@Database(entities = {Word.class}, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
     public abstract WordDAO wordDAO();
-    private WordRoomDatabase INSTANCE;
-    public static WordRoomDatabase getDatabase(Context context){
-        synchronized (WordRoomDatabase.class){
-            if(INSTANCE == null){
+
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(Context context) {
+        synchronized (WordRoomDatabase.class) {
+            if (INSTANCE == null) {
                 INSTANCE = Room.databaseBuilder(context, WordRoomDatabase.class, " WordRoomDatabase").fallbackToDestructiveMigration().allowMainThreadQueries().build();
             }
         }
